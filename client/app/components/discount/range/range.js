d47be1522e68a61e8f209ac437383f156d5bd5fc
xiaoshude <fzyde123@gmail.com>
2016-10-14 16:46:14 +0800

    添加导入状态

--- client/app/components/discount/range/range.js
+++ client/app/components/discount/range/range.js
@@ -3,22 +3,35 @@ import editRangeComponent from './edit/range.component.js';
 import detailRangeComponent from './detail/range.component.js';
 
 let rangeModule = angular.module('range', [
-    uiRouter
+  uiRouter
 ])
-    .config(($stateProvider) => {
-        "ngInject";
-        $stateProvider
-            .state('discount.editrange', {
-                url: '/discount/editrange/:id',
-                template: '<editrange></editrange>'
-            })
-            .state('discount.detailrange', {
-            url: '/discount/detailrange/:id',
-            template: '<detailrange></detailrange>'
-          });
-    })
+  .config(($stateProvider) => {
+    "ngInject";
+    $stateProvider
+      .state('discount.editrange', {
+        url: '/discount/editrange/:id',
+        template: '<editrange></editrange>'
+      })
+      .state('discount.detailrange', {
+        url: '/discount/detailrange/:id',
+        template: '<detailrange></detailrange>'
+      });
+  })
+  .filter('importStatusTrs', function () {
+    return function (input) {
+      let output = '导入开始';
+      switch (input) {
+        case 1:
+          output = '正在导入';
+          break;
+        case 2:
+          output = '导入完成'
+      }
+      return output;
+    }
+  })
 
-    .component('editrange', editRangeComponent)
-    .component('detailrange', detailRangeComponent);
+  .component('editrange', editRangeComponent)
+  .component('detailrange', detailRangeComponent);
 
 export default rangeModule;
